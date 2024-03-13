c4fde7276d6b4fc62dde7b77aa326e5ca5d30921
pattersonrptr <pattersonjunior@gmail.com>
2024-03-13 18:19:43 -0300

    removed unused file

--- src/infra/db/entities/todo.py
+++ src/infra/db/entities/todo.py
@@ -4,10 +4,9 @@ from src.infra.db.settings.base import Base
 
 class ToDo(Base):
     __tablename__ = "todo"
-    
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String(length=255), nullable=False)
 
     def __repr__(self):
         return f"ToDo [id={self.id}, title={self.title}]"
-    
