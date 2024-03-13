a442645773fb670499c1165c6062e9cc0347ba05
pattersonrptr <pattersonjunior@gmail.com>
2024-03-13 18:15:04 -0300

    :octocat: Let's get started

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.10.12
+
+EXPOSE 5000
+
+WORKDIR /app
+
+COPY requirements.txt /app
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD python run.py
+# CMD python3 -m http.server 5000
---
+++ src/infra/db/entities/todo.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, Integer
+from src.infra.db.settings.base import Base
+
+
+class ToDo(Base):
+    __tablename__ = "todo"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(length=255), nullable=False)
+
+    def __repr__(self):
+        return f"ToDo [id={self.id}, title={self.title}]"
+    
